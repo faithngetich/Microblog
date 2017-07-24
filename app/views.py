1fc726adf7326afcfcf0b48f1cd8b4fdc6ee32ca
faithngetich <faith.ngetich@andela.com>
2017-07-24 18:58:06 +0300

    add views

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from app import app
 from flask import render_template
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname' : 'Miguel'}
-    return render_template('index.html',
-                            title='Home',
-                            user=user)
+# index function supressed for brevity
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm
+    return render_template('login.html',
+                            title='SIgn In',
+                            form=form)
